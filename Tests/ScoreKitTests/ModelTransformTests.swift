080776ed62912a40d04a2a9017ee7ba7a04057c9
Benedikt Eickhoff <mail@benedikt-Eickhoff.de>
2025-10-05 18:33:32 +0200

    feat(model): add Notation types and transforms for slur, hairpin, articulation; extend LilyEmitter to output spanners and articulations; add tests

---
+++ Tests/ScoreKitTests/ModelTransformTests.swift
@@ -0,0 +1,43 @@
+import XCTest
+@testable import ScoreKit
+
+final class ModelTransformTests: XCTestCase {
+    func testSlurTransformEmitsParens() throws {
+        let base: [NotatedEvent] = [
+            .init(base: .note(pitch: Pitch(step: .C, alter: 0, octave: 4), duration: Duration(1, 4))),
+            .init(base: .note(pitch: Pitch(step: .D, alter: 0, octave: 4), duration: Duration(1, 4))),
+            .init(base: .note(pitch: Pitch(step: .E, alter: 0, octave: 4), duration: Duration(1, 4)))
+        ]
+        let voice = Voice(events: base)
+        let (v2, _) = Transform.addSlur(to: voice, start: 0, end: 2)
+        let ly = LilyEmitter.emit(notated: v2.events, title: "Slur Test")
+        print("SLUR_LY=\n\(ly)")
+        XCTAssertTrue(ly.contains("c'4("))
+        XCTAssertTrue(ly.contains("e'4)"))
+    }
+
+    func testHairpinTransformEmitsSpanners() throws {
+        let base: [NotatedEvent] = [
+            .init(base: .note(pitch: Pitch(step: .C, alter: 0, octave: 4), duration: Duration(1, 4))),
+            .init(base: .note(pitch: Pitch(step: .D, alter: 0, octave: 4), duration: Duration(1, 4))),
+            .init(base: .note(pitch: Pitch(step: .E, alter: 0, octave: 4), duration: Duration(1, 4)))
+        ]
+        let voice = Voice(events: base)
+        let (v2, _) = Transform.applyHairpin(to: voice, start: 0, end: 2, type: .crescendo)
+        let ly = LilyEmitter.emit(notated: v2.events, title: "Hairpin Test")
+        print("HAIRPIN_LY=\n\(ly)")
+        XCTAssertTrue(ly.contains("c'4 \\<"))
+        XCTAssertTrue(ly.contains("e'4 \\!"))
+    }
+
+    func testArticulationTransformEmitsSuffix() throws {
+        let base: [NotatedEvent] = [
+            .init(base: .note(pitch: Pitch(step: .C, alter: 0, octave: 4), duration: Duration(1, 4)))
+        ]
+        let voice = Voice(events: base)
+        let (v2, _) = Transform.addArticulation(to: voice, index: 0, articulation: .staccato)
+        let ly = LilyEmitter.emit(notated: v2.events, title: "Articulation Test")
+        print("ARTIC_LY=\n\(ly)")
+        XCTAssertTrue(ly.contains("c'4-."))
+    }
+}
